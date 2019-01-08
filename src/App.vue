8c829e7d2e513a4800e97f2a35e4eda526da96e4
Javier Llerenas <javr.llerenas@gmail.com>
2019-01-07 22:38:56 -0800

    create fixed topnav

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
     <!-- nav -->
-    <secition id="topnav"></secition>
+    <section id="topnav">
+      <div class="topnav__wrapper">
+        <figure class="topnav__logo-wrapper">
+          <img src="@/assets/imgs/jlogo.svg" alt="javi-logo">
+        </figure>
+        <figure class="topnav__hamburger">
+          <font-awesome-icon icon="bars"/>
+        </figure>
+      </div>
+    </section>
 
     <main>
-      <!-- hero -->
-      <section class="hero">
+      <!-- home -->
+      <section class="home">
         <h1 class="title">hello</h1>
       </section>
 
       <!-- skills -->
-      <section class="kills"></section>
+      <section class="kills hero"></section>
 
       <!-- projects -->
-      <section class="projects"></section>
+      <section class="projects hero"></section>
 
       <!-- contact -->
       <section class="contact"></section>
@@ -34,8 +43,28 @@ export default class App extends Vue {}
 </script>
 
 <style lang="less">
+@import "./assets/styles/styles.less";
 
-.section {
+.hero {
   width: 100vw;
+  height: 100vh;
+}
+
+// topnav
+#topnav {
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  background-color: @blue-secondary;
+  background-image: linear-gradient(180deg, #00bdfe 25%, #007afe 100%);
+
+  .topnav__wrapper {
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 }
 </style>
