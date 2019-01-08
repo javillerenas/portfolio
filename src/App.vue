c7fb85343943412a90e4ae9b150fbecb2f7d9a8b
Javier Llerenas <javr.llerenas@gmail.com>
2019-01-07 22:01:15 -0800

    fix: main.less import

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- nav -->
+    <secition id="topnav"></secition>
+
+    <main>
+      <!-- hero -->
+      <section class="hero">
+        <h1 class="title">hello</h1>
+      </section>
+
+      <!-- skills -->
+      <section class="kills"></section>
+
+      <!-- projects -->
+      <section class="projects"></section>
+
+      <!-- contact -->
+      <section class="contact"></section>
+
+      <!-- footer -->
+      <footer class="footer"></footer>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.section {
+  width: 100vw;
 }
 </style>
