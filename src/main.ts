ecc718e51b45af9dab1d1513a389fc785a332b53
Javier Llerenas <javr.llerenas@gmail.com>
2019-01-07 20:49:48 -0800

    chore: add regular FA lib

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import fontawesome from '@fortawesome/fontawesome';
-import faUser from '@fortawesome/fontawesome-free-solid/faUser';
-import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook';
+import { fas } from '@fortawesome/fontawesome-free-solid';
+import { fab } from '@fortawesome/fontawesome-free-brands';
+import { far } from '@fortawesome/fontawesome-free-regular';
 
-fontawesome.library.add(faUser)
-fontawesome.library.add(faFacebook)
+fontawesome.library.add(fas, fab, far);
 
 Vue.config.productionTip = false;
 
