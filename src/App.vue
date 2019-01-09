edbebdeb5884acc471d45668380925c7acb42de4
Javier Llerenas <javr.llerenas@gmail.com>
2019-01-09 03:29:15 -0800

    main: finish main section

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,38 @@
         <figure class="topnav__logo-wrapper">
           <img src="@/assets/imgs/jlogo.svg" alt="javi-logo">
         </figure>
-        <figure class="topnav__hamburger">
-          <font-awesome-icon icon="bars"/>
+        <figure class="topnav__hamburger flex-center">
+          <font-awesome-icon :icon="['fas', 'bars']"/>
         </figure>
       </div>
     </section>
 
     <main>
       <!-- home -->
-      <section class="home">
-        <h1 class="title">hello</h1>
+      <section id="home" class="home">
+        <div class="home__wrapper">
+          <figure class="home__figure">
+            <img class="home__profile" src="@/assets/imgs/profile-circle.png" alt>
+          </figure>
+          <figure class="home__name">
+            <img src="@/assets/imgs/name.svg" alt="Javier Llerenas">
+          </figure>
+          <div class="home__text">
+            <p>
+              I am an energetic <span class="is-orange">Computer Scientist</span>
+              who loves <span class="is-red">good design</span>
+              and creating <span class="is-primary">beautiful web apps</span>.
+            </p>
+            <p>
+              Located in Las Vegas, NV.
+              I specialize in <span class="is-green">Serverless Front End + Design</span>.
+            </p>
+          </div>
+          <div class="home__buttons">
+            <button class="btn btn-red">Contact me!</button>
+            <button class="btn btn-blue">Resume</button>
+          </div>
+        </div>
       </section>
 
       <!-- skills -->
@@ -50,11 +72,15 @@ export default class App extends Vue {}
   height: 100vh;
 }
 
+@topnav-height: 6rem;
 // topnav
 #topnav {
   width: 100vw;
+  height: @topnav-height;
   position: fixed;
   top: 0;
+  display: flex;
+  align-items: center;
   background-color: @blue-secondary;
   background-image: linear-gradient(180deg, #00bdfe 25%, #007afe 100%);
 
@@ -65,6 +91,68 @@ export default class App extends Vue {}
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    .topnav__hamburger {
+      background: @dark-grey;
+      color: white;
+      width: 4rem;
+      height: 4rem;
+      border-radius: 4px;
+      border: 1px solid @background-light;
+      font-size: 2rem;
+    }
+  }
+}
+
+// home
+#home {
+  margin-top: @topnav-height;
+  height: calc(100vh - @topnav-height);
+  // border: 1px solid red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .home__wrapper {
+    display: flex;
+    flex-direction: column;
+
+    .home__figure {
+      height: 25rem;
+      text-align: center;
+
+      .home__profile {
+        max-width: calc(20rem + 10vw);
+        border-radius: 50%;
+        box-shadow: @dark-shadow;
+      }
+    }
+
+    .home__name {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .home__text {
+      text-align: center;
+      margin-top: 1rem;
+    }
+
+    .home__buttons {
+      margin-top: 1rem;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+
+      > button {
+        height: 5rem;
+        
+        &:first-child {
+          margin-bottom: 2rem;
+        }
+      }
+    }
   }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import '@/assets/styles/main.less';
 // font awesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(fas, fab );
+library.add(fas, fab, far);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
